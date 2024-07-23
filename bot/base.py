a248c0d23cc9f256a93c88549f62b92969f8de70
Krishna <96517910+NrTeams@users.noreply.github.com>
2024-07-23 18:48:21 +0530

    Update base.py

--- bot/base.py
+++ bot/base.py
@@ -1,5 +1,7 @@
 from bot.__init__ import create_client
 from bot.config import TG_CONFIG
+import pyrogram
+
 logging.basicConfig(
     format="[%(asctime)s] [%(levelname)s] - %(message)s",
     datefmt="%d-%b-%y %I:%M:%S %p",
