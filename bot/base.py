1469ae0807a93295c068caf298db6b9669c3c078
Krishna <96517910+NrTeams@users.noreply.github.com>
2024-07-23 18:46:36 +0530

    Update base.py

--- bot/base.py
+++ bot/base.py
@@ -1,6 +1,16 @@
 from bot.__init__ import create_client
 from bot.config import TG_CONFIG
+logging.basicConfig(
+    format="[%(asctime)s] [%(levelname)s] - %(message)s",
+    datefmt="%d-%b-%y %I:%M:%S %p",
+    level=logging.INFO,
+    handlers=[logging.FileHandler(log.txt), logging.StreamHandler()]
+)
 
+# Set logging level for pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+LOGGER = logging.getLogger(__name__)
 from bot import create_client
 
 client = create_client(TG_CONFIG.stringhi)
